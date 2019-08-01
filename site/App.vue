d39ce1e2f225d2ebea7f60f6cccec33874dd2104
zxw <you@example.com>
2019-08-01 14:55:40 +0800

    自定义npm组件

---
+++ site/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to zxw-test-component'
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  padding-bottom: 200px;
+}
+</style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mod-header">
+    Header {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      title: 'title'
+    }
+  }
+}
+</script>
+
